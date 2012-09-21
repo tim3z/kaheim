7e53ff08165a1faa0659d56b83e03e5444f5dc77
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2012-09-21 08:15:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JeKaWg::Application.config.secret_token = '8ed6f3a18aef7b45705b590dcbe814ee4ea4fd2ebe655f61cd2e034325bc42c356da204e2344b5392ae21177632da7698db11773f657bd3e98a10e3192dc8c43'
