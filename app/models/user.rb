7b620d0b44e659eee867e3ffe4c365bb779e12b0
Tim Zeitz <dev.tim.zeitz@gmail.com>
2012-11-08 09:27:29 +0100

    fix typo in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
 
-  has_many :offers, dependant: :destroy
-  has_many :requests, dependant: :destroy
+  has_many :offers, dependent: :destroy
+  has_many :requests, dependent: :destroy
 end
