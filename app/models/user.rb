6334fbe4638a0af2c4c957ffebc61119bc05112a
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2014-03-06 18:26:21 +0100

    edit user link

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  #TODO attr_accessible :email, :password, :password_confirmation, :remember_me, :name
-
   validates_presence_of :name
 
 
