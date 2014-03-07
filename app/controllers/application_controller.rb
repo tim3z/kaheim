3b08c3c33e1ee552f56f8dbc733a0d1990a774a7
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2014-03-07 12:06:28 +0100

    language selection - issue #13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def authenticate_admin_user!
     authenticate_user!
@@ -18,5 +19,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    (I18n.locale == :de) ? {} : {locale: I18n.locale}
   end
 end
