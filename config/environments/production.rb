ee37ced52fdc4e58b8badb6e7f4ec784dfb13c24
Matthias Grundmann <matthias@glasmail.de>
2014-08-20 18:12:47 +0200

    Use sendmail to send mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,17 +85,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'kaheim.de' }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port:  587,
-    domain: 'kaheim.de',
-    user_name: 'jekawg@gmail.com',
-    password: Rails.application.secrets.mail,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Deliver mails on exceptions
   config.middleware.use ExceptionNotification::Rack, email: {
