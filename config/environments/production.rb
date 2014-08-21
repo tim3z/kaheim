99870035ffec6f8a7a85fb068c1961d03d88ec93
Matthias Grundmann <matthias@glasmail.de>
2014-08-21 12:11:14 +0200

    Don't raise delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'kaheim.de' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
 
   # Deliver mails on exceptions
   config.middleware.use ExceptionNotification::Rack, email: {
