481088806dd36d4cd1a470a53529aaf800185836
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2014-08-21 15:17:34 +0200

    adding missing validations - #15661506405826

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :title, :description, :user
+  validates_presence_of :title, :description, :user, :gender
 
   scope :current, -> { where('requests.updated_at >= ?', 31.days.ago) }
   scope :outdated, -> { where('requests.updated_at < ?', 31.days.ago) }
