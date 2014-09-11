d42d4f654b3048e6249c4dad1b3f34b3c212740a
Matthias Grundmann <matthias@glasmail.de>
2014-09-11 21:36:16 +0200

    Test new item notifications for subscriber if user is unlocked

---
+++ test/controllers/admin/users_controller_test.rb
@@ -0,0 +1,57 @@
+class Admin::UsersControllerTest < ActionController::TestCase
+
+  def setup
+    assert users(:admin).admin
+    sign_in(users(:admin))
+    request.env['HTTP_REFERER'] = 'localhost'
+  end
+
+  test 'unlocking user with offer sends notifications to subscribers' do
+    user = users(:tina)
+    assert_not user.unlocked
+
+    assert_difference 'ActionMailer::Base.deliveries.size', +2 do
+      put :unlock, id: user.id
+    end
+
+    notification_email = ActionMailer::Base.deliveries.last
+    assert_equal '[Kaheim] ' + I18n.t('subscriptions.new_item_notification.subject'), notification_email.subject
+    assert_match(/.*wurde ein neues Angebot eingetragen.*/, notification_email.body.to_s)
+
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+  test 'unlocking user with request sends notifications to subscribers' do
+    user = users(:jim)
+    assert_not user.unlocked
+
+    assert_difference 'ActionMailer::Base.deliveries.size', +2 do
+      put :unlock, id: user.id
+    end
+
+    notification_email = ActionMailer::Base.deliveries.last
+    assert_equal '[Kaheim] ' + I18n.t('subscriptions.new_item_notification.subject'), notification_email.subject
+    assert_match(/.*wurde ein neues Gesuch eingetragen.*/, notification_email.body.to_s)
+
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+  test 'unlocking user with two offers sends notifications to subscribers' do
+    user = users(:john)
+    assert_not user.unlocked
+
+    assert_difference 'ActionMailer::Base.deliveries.size', +4 do
+      put :unlock, id: user.id
+    end
+
+    notification_email = ActionMailer::Base.deliveries.last
+    assert_equal '[Kaheim] ' + I18n.t('subscriptions.new_item_notification.subject'), notification_email.subject
+    assert_match(/.*wurde ein neues Angebot eingetragen.*/, notification_email.body.to_s)
+
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+end
