44da251fda6c5739ae3194a180412ed5aeee2cf1
Matthias Grundmann <matthias@glasmail.de>
2014-09-11 12:20:52 +0200

    Fix 'Unable to autoload constant CustomBootstrapFormBuilder'

--- app/form_builders/custom_bootstrap_form_builder.rb
+++ app/form_builders/custom_bootstrap_form_builder.rb
@@ -1,19 +1,17 @@
-module Kaheim
-  class CustomBootstrapFormBuilder < BootstrapForm::FormBuilder
-    def prepend_and_append_input(options, &block)
-      options = options.extract!(:prepend, :append, :input_group_wrapper_class)
-      input = capture(&block)
+class CustomBootstrapFormBuilder < BootstrapForm::FormBuilder
+  def prepend_and_append_input(options, &block)
+    options = options.extract!(:prepend, :append, :input_group_wrapper_class)
+    input = capture(&block)
 
-      input = content_tag(:span, options[:prepend], class: input_group_class(options[:prepend])) + input if options[:prepend]
-      input << content_tag(:span, options[:append], class: input_group_class(options[:append])) if options[:append]
-      input = content_tag(:div, input, class: "input-group #{options[:input_group_wrapper_class]}") unless options.empty?
-      input
-    end
+    input = content_tag(:span, options[:prepend], class: input_group_class(options[:prepend])) + input if options[:prepend]
+    input << content_tag(:span, options[:append], class: input_group_class(options[:append])) if options[:append]
+    input = content_tag(:div, input, class: "input-group #{options[:input_group_wrapper_class]}") unless options.empty?
+    input
+  end
 
-    def localized_text_field method, options = {}
-      val = object.send(method)
-      options[:value] = @template.l val if val
-      text_field method, options
-    end
+  def localized_text_field method, options = {}
+    val = object.send(method)
+    options[:value] = @template.l val if val
+    text_field method, options
   end
 end
