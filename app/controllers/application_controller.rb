6c7474b09a35c21b5f44a39c690b1a09ca667e88
Matthias Grundmann <matthias@glasmail.de>
2014-09-23 11:35:14 +0200

    Redirect user to original page after login #16351842281304

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,25 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
+  after_filter :store_location
 
   def authenticate_admin_user!
     authenticate_user!
     unless current_user.admin?
       flash[:alert] = "Unauthorized Access!"
       redirect_to root_path
+    end
+  end
+
+  def store_location
+    if request.path != new_user_session_path &&
+      request.path != new_user_registration_path &&
+      request.path != new_user_password_path &&
+      request.path != edit_user_password_path &&
+      request.path != user_confirmation_path &&
+      request.path != destroy_user_session_path &&
+      !request.xhr? # don't store ajax calls
+      store_location_for(:user, request.original_url)
     end
   end
 
