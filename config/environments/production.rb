3c741722c915b26ad94da39c27646bae8d2cca9f
Matthias Grundmann <matthias@glasmail.de>
2014-10-01 18:26:44 +0200

    Insert link to new item in new item notification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'kaheim.de' }
+  config.action_mailer.default_url_options = { host: 'kaheim.de', protocol: 'https' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
 
