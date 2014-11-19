8fb0b457558f47414e41c3371d37e163d79f6125
Matthias Grundmann <matthias@glasmail.de>
2014-11-19 22:37:23 +0100

    Fix tests for notifications when unlocking users

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -6,8 +6,8 @@ class Admin::UsersControllerTest < ActionController::TestCase
     request.env['HTTP_REFERER'] = 'localhost'
   end
 
-  test 'unlocking user with offer sends notifications to subscribers' do
-    user = users(:tina)
+  test 'unlocking confirmed user with offer sends notifications to subscribers' do
+    user = users(:richard)
     assert_not user.unlocked
 
     num_subscribers = Subscription.offers.confirmed.count
@@ -23,8 +23,8 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert user.unlocked
   end
 
-  test 'unlocking user with request sends notifications to subscribers' do
-    user = users(:jim)
+  test 'unlocking confirmed user with request sends notifications to subscribers' do
+    user = users(:rachel)
     assert_not user.unlocked
 
     num_subscribers = Subscription.offers.confirmed.count
@@ -40,8 +40,8 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert user.unlocked
   end
 
-  test 'unlocking user with two offers sends notifications to subscribers' do
-    user = users(:john)
+  test 'unlocking confirmed user with two offers sends notifications to subscribers' do
+    user = users(:lisa)
     assert_not user.unlocked
 
     num_subscribers = Subscription.offers.confirmed.count
@@ -49,9 +49,45 @@ class Admin::UsersControllerTest < ActionController::TestCase
       put :unlock, id: user.id
     end
 
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+  test "unlocking unconfirmed user with offer doesn't send notifications to subscribers" do
+    user = users(:tina)
+    assert_not user.unlocked
+
+    assert_no_difference 'ActionMailer::Base.deliveries.size' do
+      put :unlock, id: user.id
+    end
+
     notification_email = ActionMailer::Base.deliveries.last
     assert_equal '[Kaheim] ' + I18n.t('subscriptions.new_item_notification.subject'), notification_email.subject
     assert_match(/.*wurde ein neues Angebot eingetragen.*/, notification_email.body.to_s)
+
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+  test "unlocking unconfirmed user with request doesn't send notifications to subscribers" do
+    user = users(:jim)
+    assert_not user.unlocked
+
+    assert_no_difference 'ActionMailer::Base.deliveries.size' do
+      put :unlock, id: user.id
+    end
+
+    user = User.find(user.id)
+    assert user.unlocked
+  end
+
+  test "unlocking unconfirmed user with two offers doesn't send notifications to subscribers" do
+    user = users(:john)
+    assert_not user.unlocked
+
+    assert_no_difference 'ActionMailer::Base.deliveries.size' do
+      put :unlock, id: user.id
+    end
 
     user = User.find(user.id)
     assert user.unlocked
