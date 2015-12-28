3173917aaadccdcda4458ed2ce2401b1307c56b6
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2015-12-28 14:29:50 +0100

    turn item into concern - some code polishing

---
+++ app/models/concerns/item.rb
@@ -0,0 +1,49 @@
+require 'active_support/concern'
+
+module Item
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :user
+    validates_presence_of :user
+    has_many :answers, as: :item, dependent: :destroy
+    has_one :item_reactivator, as: :item, dependent: :destroy
+
+    scope :unlocked, -> { joins(:user).where(users: { unlocked: true }) }
+    scope :locked, -> { joins(:user).where(users: { unlocked: false }) }
+    scope :confirmed, -> { joins(:user).where.not(users: { confirmed_at: nil }) }
+    scope :current, -> { where("#{table_name}.updated_at >= ?", outdating_date) }
+    scope :outdated, -> { where("#{table_name}.updated_at < ?", outdating_date) }
+    scope :is_public, -> { where(is_public: true) }
+  end
+
+  module ClassMethods
+    def outdating_date
+      31.days.ago
+    end
+
+    def visible_for(user = nil)
+      return current.or(where(user: user)) if user.try :admin?
+      query = current.unlocked.confirmed.is_public
+      query = query.or(where(user: user)) if user
+      query
+    end
+  end
+
+  def current?
+    updated_at >= self.class.outdating_date
+  end
+
+  def outdated?
+    updated_at < self.class.outdating_date
+  end
+
+  def visible?
+    user.unlocked? && user.confirmed? && current?
+  end
+
+  def get_or_create_reactivator
+    return item_reactivator if item_reactivator
+    ItemReactivator.create! item: self
+  end
+end
