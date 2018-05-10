9b3ccdcd3245626838d07d2f94b0046890d82ee7
Matthias Grundmann <matthias@glasmail.de>
2018-05-10 23:57:24 +0200

    Read ruby version for deployment from .ruby-version

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,9 @@ set :linked_files, %w{.env}
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+rubyversion = File.open('.ruby-version', &:readline).gsub(/\n/, '')
 # Default value for default_env is {}
-set :default_env, { path: "/package/host/localhost/ruby-2.3/bin:$PATH" }
+set :default_env, { path: "/package/host/localhost/#{rubyversion}/bin:$PATH" }
 
 # Default value for keep_releases is 5
 set :keep_releases, 20
