614d41f2cea3626ab8bbae0cbecc05ee973a7128
Matthias Grundmann <matthias@glasmail.de>
2018-06-10 17:43:11 +0200

    protect_from_forgery: Use :null_session instead of :exception as strategy
    As long as no filters are prepended this should still be ok security-wise
    see https://www.sourceclear.com/blog/When-Rails-protect_from_forgery-Fails/
    https://github.com/rails/rails/issues/21948

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by ignoring the session
+  protect_from_forgery
   delegate :tm, to: :view_context
 
   before_action :configure_permitted_parameters, if: :devise_controller?
