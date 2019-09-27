70bfae169edab851d043b661ac7f75fa6775d87f
Matthias Grundmann <matthias@glasmail.de>
2019-09-27 22:15:51 +0200

    Add button to admin dashboard to resend owner link for item #29

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -13,6 +13,7 @@ ActiveAdmin.register_page "Dashboard" do
             column I18n.t('active_admin.actions.title') do |offer|
               link_to(I18n.t('active_admin.actions.show'), offer) + ' ' +
               link_to(offer.blocked ? 'Freischalten' : 'Sperren', offer.blocked ? unblock_admin_offer_path(offer) : block_admin_offer_path(offer), method: :put) + ' ' +
+              link_to(I18n.t('items.request_edit_link.send_link_again'), request_owner_link_offer_path(offer, email: offer.email), method: :post) + ' ' +
               link_to(I18n.t('active_admin.actions.show_in_admin_panel'), admin_offer_path(offer))
             end
           end
