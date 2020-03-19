0483c5d81abc732c66ae6db7cca28cc9d9465126
Matthias Grundmann <matthias@glasmail.de>
2020-03-19 21:21:15 +0100

    Enforce a space inside description to stop simple spam

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,5 +4,7 @@ class Request < ApplicationRecord
   validates_presence_of :title, :description, :gender
   validates_length_of :title, maximum: 140
 
+  validates :description, format: { with: /.+\s.+/, message: "at least two words" }
+
   enum gender: { dontcare: 0, female: 1, male: 2 }
 end
