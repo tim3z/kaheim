684686abeecea874e776468c928795df4c26d54c
Matthias Grundmann <matthias@glasmail.de>
2020-07-05 22:08:28 +0200

    Use versioned selector

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Dkim::domain      = 'kaheim.de'
-  Dkim::selector    = 'mail'
+  Dkim::selector    = 'mail.2020-06'
   Dkim::private_key = open('/home/kaheim7/dkim/kaheim.de.2020-06.private').read
 
   ActionMailer::Base.register_interceptor(Dkim::Interceptor)
